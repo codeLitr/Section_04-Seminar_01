f216792f949cc1210a2663ae8d4147ecb6750581
SMV <kraft088@gmail.com>
2022-12-16 22:32:37 +1100

    Add Task 9

---
+++ Seminar_01/Task_09/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 9.
+Вычислить числа Фиббоначи.
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    bool isNumber = int.TryParse(strValue, out int value);
+    if (isNumber)
+    {
+        return value;
+    }
+    throw new Exception("Данное значение невозможно преобразовать в целое число");
+}
+int value = Prompt("Введите число > ");
+if (value < 0)
+{
+    Console.WriteLine("Число должно быть с нуля");
+}
+else
+{
+    Console.WriteLine($"Номер числа последовательности Фиббоначи {value} Значение числа -> {Fibbonachi(value)}");
+}
+
+int Fibbonachi(int value)
+{
+    int fibbonachi = 0;
+    int tempVar1 = 0;
+    int tempVar2 = 1;
+    for (int i = 0; i <= value; i++)
+    {
+        fibbonachi = tempVar1 + tempVar2;
+        tempVar1 = tempVar2;
+        tempVar2 = fibbonachi;
+    }
+    return tempVar1;
+}
