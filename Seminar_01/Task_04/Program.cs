740baeda0e3dfe393db7b3826e914f28653ed5c9
SMV <kraft088@gmail.com>
2022-12-16 22:14:21 +1100

    Add Task 4

---
+++ Seminar_01/Task_04/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 3.
+Напишите программу вычисления функции куба: 
+x = f(a) 
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int value = Prompt("Введите число > ");
+Console.WriteLine(value * value * value);
