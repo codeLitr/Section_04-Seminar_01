860b2e3ed1eeffde12a6c4287878c7e97fee402c
SMV <kraft088@gmail.com>
2022-12-16 22:11:37 +1100

    Add Task 2

---
+++ Seminar_01/Task_02/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 2.
+Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 5; b = 25 -> да
+a = 2; b = 10 -> нет
+a = 9; b = -3 -> нет
+a = -3; b = 9 -> да
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue;
+    strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int firstValue = Prompt("Введите первое число > ");
+int secondValue = Prompt("Введите второе число > ");
+if (firstValue * firstValue == secondValue)
+{
+    Console.WriteLine("-> Да");
+}
+else
+{
+    Console.WriteLine("-> Нет");
+}
