072bb7d38dfade9be807f48ee63471b76832ca58
SMV <kraft088@gmail.com>
2022-12-16 22:23:47 +1100

    Add Task 6

---
+++ Seminar_01/Task_06/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 6.
+Напишите программу вычисления модуля числа.
+2 -> 2
+-3 -> 3
+-7 -> 7
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int value = Prompt("Введите число > ");
+int result;
+if (value < 0)
+{
+    result = -value;
+}
+else
+{
+    result = value;
+}
+
+Console.WriteLine($"{value} -> {result}");
