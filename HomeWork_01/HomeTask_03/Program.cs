3ec25fa17b52d8dbc7af57948ef7bf7c81ca8399
SMV <kraft088@gmail.com>
2022-12-16 22:39:50 +1100

    Add HomeTask 3

---
+++ HomeWork_01/HomeTask_03/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Task_03.
+Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string strValue;
+    strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int A = Prompt("Введите число для проверки > ");
+
+if(A % 2 == 0)
+{
+    Console.WriteLine("Введенное число четное");
+}
+
+else
+{
+    Console.WriteLine("Введенное число нечетное");
+}
