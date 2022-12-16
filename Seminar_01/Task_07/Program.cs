501b907c0d5058d02bac12524b6be78c4fe5bba6
SMV <kraft088@gmail.com>
2022-12-16 22:27:31 +1100

    Add Task 7

---
+++ Seminar_01/Task_07/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 7.
+Напишите программу, которая принимает на вход значения и показывает его обратное значение.
+1 -> 1
+2 -> 0,5
+0,25 -> 4
+*/
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    double value = double.Parse(strValue);
+    return value;
+}
+
+double value = Prompt("Введите число > ");
+
+if (value == 0)
+{
+    Console.WriteLine("Нельзя делить на ноль");
+}
+else
+{
+    double result = 1 / value;
+    Console.WriteLine($"{value} -> {result}");
+}
