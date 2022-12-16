2896557c754f9127f6d03ffadd8b749c592f2608
SMV <kraft088@gmail.com>
2022-12-16 22:29:28 +1100

    Add Task 8

---
+++ Seminar_01/Task_08/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 8.
+Вычислите факториал числа.
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int value = Prompt("Введите число > ");
+if(value < 0)
+{
+    Console.WriteLine("Число должно быть больше нуля");
+}
+else
+{
+    Console.WriteLine($"Факториал числа {value} равен -> {Factorial(value)}");
+}
+int Factorial(int value)
+{
+    int factorial = 1;
+    for (int i = 1; i <= value; i++)
+    {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
